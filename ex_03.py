2e12f6b94426c25d3b46a648d69603fabc884eae
Maria Brandão <162699125+MariaBrandao18@users.noreply.github.com>
2024-11-05 14:50:38 -0300

    Update ex_03.py

--- ex_03.py
+++ ex_03.py
@@ -2,16 +2,21 @@
 #Aluna: Júlia Coelho Rodrigues             RA: 22408388
 
 import turtle as tl
+import time
 
 class Ponto():
-    def __init__(self, x, y):
+    def _init_(self, x, y):
         self.x = x
         self.y = y
     def goto_ponto(self):
+        time.sleep(3)
         tl.penup()
         tl.goto(self.x, self.y)
 
 p1 = Ponto(90,40)
 Ponto.goto_ponto(p1)
+
+p2 = Ponto(-25, -75)
+Ponto.goto_ponto(p2)
 
 tl.done()
