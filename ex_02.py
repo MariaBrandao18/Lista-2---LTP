64d22a596994054c6f7ebb888f0b3d4c68d27ff4
Maria Brandão <162699125+MariaBrandao18@users.noreply.github.com>
2024-11-05 14:21:35 -0300

    Create ex_02.py
    Crie classes Motor, Pneu, Veiculo, onde Veiculo herda tanto de Motor quanto de Pneu. Ambas as classes base (Motor e Pneu) devem ter um método chamado status() que retorna uma string. A classe Veiculo deve também ter um método status() que chama os métodos status() das classes base. Implemente a herança de modo que a classe Veiculo resolva o método status() de maneira correta.

---
+++ ex_02.py
@@ -0,0 +1,29 @@
+#Aluna: Maria Eduarda Rodrigues Brandão    RA: 22402382
+#Aluna: Júlia Coelho Rodrigues             RA: 22408388
+
+# Classe Motor
+class Motor:
+
+    def status(self):
+        return "Motor: em boas condições."
+    
+# Classe Pneu
+class Pneu:
+
+    def status(self):
+        return "Pneus: com pressão adequada."
+    
+# Classe Veículo, que herda de Motor e Pneu
+class Veiculo(Motor, Pneu):
+
+    def status(self):
+        # Chamando o metodo status de Motor e Pneu
+        status_motor = Motor.status(self)
+        status_pneu = Pneu.status(self)
+        return f"{status_motor}\n{status_pneu}"
+    
+# Testando as classes
+if __name__ == "__main__":
+    
+    carro = Veiculo()
+    print(carro.status())
